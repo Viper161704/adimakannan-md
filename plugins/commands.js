c0c0be0bbc85fb61f5daa65f13faca98b7727142
Adarsh <103887234+Viper161704@users.noreply.github.com>
2022-05-23 10:57:05 +0530

    Update commands.js

--- plugins/commands.js
+++ plugins/commands.js
@@ -13,7 +13,7 @@ const {
     FancyRandom: FancyRandom,
     getListFromCommand: getListFromCommand,
     skbuffer: skbuffer
-} = require("raganork-bot");
+} = require("ᴀᴅɪᴍᴀ ᴋᴀɴɴᴀɴ");
 let w = "public" != MODE;
 const readMore = String.fromCharCode(8206).repeat(4001);
 Module({
